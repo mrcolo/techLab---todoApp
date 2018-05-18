3b209d260f0b1b198c6a7e2c9d6cefd8d2907b2f
Francesco Colonnese <colo28197@gmail.com>
2018-05-18 13:52:16 -0700

    added db

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(bodyParser.json());
@@ -8,6 +9,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'jade');
 
+const dbURL = "mongodb://fcolo:hellotechlab@ds125906.mlab.com:25906/todoapp"
+const Schema = mongoose.Schema;
+var todo = mongoose.model('todo', new Schema({ todo: String }));
+
+mongoose.Promise = global.Promise;
+mongoose.connect(dbURL, function(err) {
+    if(err) {
+        console.log('Connection error');
+    }
+});
 //VIEWS
 //basic endpoint
 app.get('/', (req, res) => {
@@ -19,35 +30,45 @@ app.get('/', (req, res) => {
 
 //API
 //initialize Array
-var myTODOs = [];
 //get my chores
 app.get('/getTODO', (req, res) => {
-  console.log("Rendering " + myTODOs + "...");
-  res.send(myTODOs)
+	todo.find({}, function(err, docs) {
+    if (!err){
+				console.log("Rendering " + docs + "...");
+				res.send(docs);
+
+    } else {throw err;}
+});
+
 });
 
 app.post('/deleteTODO', (req, res) => {
   //delete todo at req.body.id and if length is 1 just set the element as undefined
-  if(myTODOs.length != 1){
-    
-    myTODOs.splice(req.body.id,req.body.id);
-    console.log("Deleted element number " + req.body.id);
-    res.send("Deleted element number " + req.body.id);
-    
-  }
-  else{
-    
-   myTODOs[0] = undefined;
-   res.send("Cleared Array");
-  }
-  
+	todo.deleteOne({_id: req.body.id}, function (err){
+		if(err)
+			console.log(err);
+		else{
+			res.send("Deleted" + req.body.id);
+		}
+	});
+	console.log("Deleted" + req.body.id);
+
 });
 
 //add a todo
 app.post('/addTODO', (req, res) => {
-  myTODOs.push(req.body.todo);
+
+	var temp_todo = new todo({todo: req.body.todo})
+	console.log(temp_todo)
+	temp_todo.save(function(err) {
+		if(err){
+			console.log(err)
+		}else{
+			res.send("Added " + req.body.todo)
+		}
+	})
   console.log("Added " + req.body.todo)
-  res.send("Added " + req.body.todo)
+
 });
 
 //let's start the server
